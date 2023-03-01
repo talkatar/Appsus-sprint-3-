5bab1f2ef7ea5caf5cfd7a0243fcba9b21cb488f
Tal Katar <talkatar120@gmail.com>
2023-03-01 10:12:16 +0200

    hibur hutim

--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
+import EmailIndex from './apps/mail/pages/EmailIndex.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -13,6 +15,14 @@ const routerOptions = {
 		{
 			path: '/about',
 			component: AboutUs,
+		},
+		{
+			path: '/mail',
+			component: EmailIndex,
+		},
+		{
+			path: '/keep',
+			component: NoteIndex,
 		},
 	],
 }

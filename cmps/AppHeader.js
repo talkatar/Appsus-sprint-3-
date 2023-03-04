754b486ca3133f524a45f3aefc1a128cba56d3e0
Tal Katar <talkatar120@gmail.com>
2023-03-04 14:58:15 +0200

    qweqwe

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -8,9 +8,9 @@ export default {
                 <nav>
                     <router-link to="/">Home</router-link> 
                     <router-link to="/about">About</router-link>
-                    <router-link to="/keep"><img class="keep-logo-header" src='../../../assets/img/keep.png' alt=""></router-link>
-                    <router-link to="/email"><img class="gmail-logo-header" src='../../../assets/img/Gmail-home-page.png' alt=""></router-link>
-                    <router-link to="/book"><img class="book-logo-header" src='../../../assets/img/book.png' alt=""></router-link>
+                    <router-link to="/keep"><img class="keep-logo-header" src='assets/img/keep.png' alt=""></router-link>
+                    <router-link to="/email"><img class="gmail-logo-header" src='assets/img/Gmail-home-page.png' alt=""></router-link>
+                    <router-link to="/book"><img class="book-logo-header" src='assets/img/book.png' alt=""></router-link>
 
                 </nav>
             </div>
--- views/HomePage.js
+++ views/HomePage.js
@@ -4,9 +4,9 @@ export default {
 <div class="links">
 
 
-<router-link  to="/keep"><img class="keep-logo" src='../../../assets/img/keep.png' alt=""></router-link>
-<router-link  to="/email"><img class="gmail-logo" src='../../../assets/img/Gmail-home-page.png' alt=""></router-link>
-<router-link  to="/keep"><img class="book-logo" src='../../../assets/img/book.png' alt=""></router-link>
+<router-link  to="/keep"><img class="keep-logo" src='assets/img/keep.png' alt=""></router-link>
+<router-link  to="/email"><img class="gmail-logo" src='assets/img/Gmail-home-page.png' alt=""></router-link>
+<router-link  to="/keep"><img class="book-logo" src='assets/img/book.png' alt=""></router-link>
 
 
 
